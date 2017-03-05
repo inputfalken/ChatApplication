ce1d2212f1a99c5915e4a212ccfea494cacf2c70
Robert Andersson <inputfalken@gmail.com>
2017-03-05 04:41:25 +0100

    Add comments

--- Protocol/Message.cs
+++ Protocol/Message.cs
@@ -23,11 +23,29 @@ namespace Protocol {
         [JsonProperty("result")]
         public string JsonObject { get; }
 
+        /// <summary>
+        /// Parses the message into it's basic form.
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
         public static Message ParseMessage(string json) => JsonConvert.DeserializeObject<Message>(json);
 
+        /// <summary>
+        /// Creates an Message with it's data with as json.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="action"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
         public static string Create<T>(Action action, T result)
             => new Message(action, JsonConvert.SerializeObject(result)).ToString();
 
+        /// <summary>
+        /// Parses the json supplied to it's object.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="json"></param>
+        /// <returns></returns>
         public static T Parse<T>(string json) => JsonConvert.DeserializeObject<T>(json);
 
         public override string ToString() => JsonConvert.SerializeObject(this);
