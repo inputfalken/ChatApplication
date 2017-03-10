66dc073722b83f6db91e090cb8bcf77978f4bc61
Robert Andersson <inputfalken@gmail.com>
2017-03-10 14:19:54 +0100

    Register window uses RX

--- ClientApplication/Register.xaml.cs
+++ ClientApplication/Register.xaml.cs
@@ -1,39 +1,49 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using Client;
+using static System.Reactive.Linq.Observable;
 
 namespace ClientApplication {
     /// <summary>
     ///     Interaction logic for Register.xaml
     /// </summary>
     public partial class Register : Window {
-        private static readonly ChatClient NetusClient = new ChatClient("10.0.2.15", 23000);
-
         public Register() {
             InitializeComponent();
-            Loaded += OnLoaded;
-            RegisterButton.Click += RegisterButtonOnClick;
+            FromEventPattern<RoutedEventArgs>(this, "Loaded")
+                .Subscribe(OnLoaded);
         }
 
-        private async void OnLoaded(object sender, RoutedEventArgs routedEventArgs) {
+        private async void OnLoaded(EventPattern<RoutedEventArgs> eventPattern) {
+            var dispatcherScheduler = new DispatcherScheduler(Dispatcher);
+            var netusClient = new ChatClient("10.0.2.15", 23000);
             try {
-                await NetusClient.Connect();
+                await netusClient.Connect();
             }
             catch (Exception) {
                 MessageBox.Show("Could not establish an connection to the Server.");
                 Close();
             }
+            FromEventPattern(RegisterButton, "Click") // Turns the event of into an observable.
+                .SelectMany(_ => netusClient.Register(UserNameBox.Text)) // Consumes the task
+                .ObserveOn(dispatcherScheduler) // Use the dispatcher for the following UI updates.
+                .Subscribe(successfulRegister => {
+                    if (successfulRegister) ProceedToMainWindow(netusClient);
+                    else Label.Content = $"{UserNameBox.Text} is taken, try with a different name";
+                });
         }
 
-        private async void RegisterButtonOnClick(object sender, RoutedEventArgs routedEventArgs) {
-            var success = await NetusClient.Register(UserNameBox.Text);
-            if (success) {
-                new MainWindow(NetusClient, UserNameBox.Text).Show();
-                Close();
-            }
-            else {
-                Label.Content = $"{UserNameBox.Text} is taken, try with a different name";
-            }
+        private void ProceedToMainWindow(ChatClient client) {
+            new MainWindow(client, UserNameBox.Text).Show();
+            Close();
         }
     }
 }
